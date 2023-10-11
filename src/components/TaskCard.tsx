9cdcb36aa7fd5cb9c20391599bdd34e0c59cbf0e
Jesse Levandovski <jesse.levandovski@outlook.com>
2023-10-11 16:50:06 -0300

    Ajuste nas datas do servidor

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -12,15 +12,14 @@ interface ITasks {
 
 export default function TaskCard({ task }: { task: ITasks }) {
     const router = useRouter();
-
     return (
-        <div         
+        <div
             className="bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer"
             onClick={() => router.push(`/tasks/edit/${task.id}`)}
         >
             <h3 className="font-bold text-2xl mb-2">{task.title}</h3>
             <p>{task.description}</p>
-            <p>{new Date(task.createdAt).toLocaleDateString()}</p>
+            <p>{new Intl.DateTimeFormat('pt-br').format(new Date(task.createdAt))}</p>
         </div>
     )
 }
