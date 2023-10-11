2fde91cbad1b6de3f9cebdaec8cb6f431e22f109
Jesse Levandovski <jesse.levandovski@outlook.com>
2023-10-11 16:15:54 -0300

    Modificações

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,11 +1,6 @@
 import { prisma } from "@/libs/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  const allTasks = await prisma.task.findMany();
-  return NextResponse.json(allTasks);
-}
-
 export async function POST(request: NextRequest) {
   const { title, description } = await request.json();
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,29 @@
 import TaskCard from "@/components/TaskCard";
+import { prisma } from "@/libs/prisma";
 
 interface ITasks {
   id: number,
   title: string,
-  description: string,
+  description: string | null,
   createdAt: string
 }
 
 
-async function loadTasks(): Promise<ITasks[]> {
-  const res = await fetch('http://localhost:3001/api/tasks', { cache: 'no-cache' })
-  const data = await res.json();
-  return data;
+async function loadTasks(): Promise<ITasks[] | any> { 
+  const allTasks = await prisma.task.findMany();
+  return allTasks;
+
 }
 
+export const revalidat = 60;
 
 export default async function HomePage() {
   const tasks = await loadTasks();
   return (
     <section className="container mx-auto">
       <div className="grid grid-cols-3 gap-3 mt-10">
-        {tasks.map((task) => (
-          <TaskCard  task={task} key={task.id}/>
+        {tasks.map((task: any) => (
+          <TaskCard task={task} key={task.id} />
         ))}
       </div>
     </section>
